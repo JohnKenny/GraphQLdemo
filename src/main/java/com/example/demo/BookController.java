069582620d9931e0b5e6a51ab34c16f57ecf48fa
JohnKenny <squareindigojohn@yahoo.ie>
2026-07-12 22:45:21 +0100

    added BooKController.

---
+++ src/main/java/com/example/demo/BookController.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BookController {
+
+}
