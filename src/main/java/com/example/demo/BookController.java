692e6af46fd8a5c53ab1aed2c84b116c417c32fd
JohnKenny <squareindigojohn@yahoo.ie>
2026-08-16 09:42:05 +0100

    BookController added

--- src/main/java/com/example/demo/BookController.java
+++ src/main/java/com/example/demo/BookController.java
@@ -8,4 +8,14 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class BookController {
 
+    @QueryMapping
+    public Book bookById(@Argument String id){
+        return Book.getById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book){
+        return Author.getById(book.authorId());
+    }
+
 }
